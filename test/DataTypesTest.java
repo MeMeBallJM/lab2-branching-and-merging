032bd200b9539c52d81f43826f0d733b0e3fc793
Joshua Morton <morton.l.joshua@gmail.com>
2026-09-21 14:37:32 -0400

    task 1 stuff

--- test/DataTypesTest.java
+++ test/DataTypesTest.java
@@ -14,11 +14,11 @@ public class DataTypesTest {
         // try removing the L and you'll see that you get an error
         // saying "Integer number too large".
         // Note: You can use _ to help make it easier to
-        //       read large numbers, as is done below.
+        // read large numbers, as is done below.
         long x = 500_000_500_000L;
         int[] numbers = new int[1_000_000];
         for (int i = 1; i <= 1_000_000; i++) {
-            numbers[i-1] = i;
+            numbers[i - 1] = i;
         }
         assertEquals(x, DataTypes.sum(numbers), "sum from 1 to 1 million should be " + x);
     }
